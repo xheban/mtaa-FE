ffeb0118d0db599b7a685d8614750e4a2e980126
JoZo <michaljozefek@gmail.com>
2020-04-24 01:23:24 +0200

    Login + prekliky na ine obrazovky resety este nefunkcne

--- app/src/main/java/com/example/mtaa/Constats.java
+++ app/src/main/java/com/example/mtaa/Constats.java
@@ -3,6 +3,9 @@ package com.example.mtaa;
 public class Constats {
 
     private static final String ROOT_URL = "http://192.168.0.116/MTAA/";
+    //private static final String ROOT_URL = "http://192.168.0.129/MTAA/"; //JOZO ip
     public static final String REGISTER_URL = ROOT_URL+"createUser.php";
+    public static final String LOGIN_URL = ROOT_URL+"login.php";
     public static final String GET_ALL_RESTAURANTS_URL = ROOT_URL+"GetAllRestaurants.php";
+
 }
