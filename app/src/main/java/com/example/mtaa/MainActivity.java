6eda47295463523c2db41f0e5c0b2aeda0fccc16
Michal Heban <michal.heban52@gmail.com>
2020-04-23 15:33:16 +0200

    delete of unused imports

--- app/src/main/java/com/example/mtaa/MainActivity.java
+++ app/src/main/java/com/example/mtaa/MainActivity.java
@@ -17,7 +17,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -25,7 +24,6 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
