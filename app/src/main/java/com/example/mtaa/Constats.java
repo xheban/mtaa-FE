45e82da87b5b11bfdc10eb2af26148e8bc2b9b61
Michal Heban <michal.heban52@gmail.com>
2020-04-23 13:08:18 +0200

    start

---
+++ app/src/main/java/com/example/mtaa/Constats.java
@@ -0,0 +1,8 @@
+package com.example.mtaa;
+
+public class Constats {
+
+    private static final String ROOT_URL = "https://192.168.0.116/MTAA/";
+    public static final String REGISTER_URL = ROOT_URL+"getFoodHistory.php";
+
+}
