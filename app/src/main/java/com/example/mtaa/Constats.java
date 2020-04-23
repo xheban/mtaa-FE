833c2a931e940ed45605d60d195b85f509588f65
Michal Heban <michal.heban52@gmail.com>
2020-04-23 16:51:41 +0200

    IP config

--- app/src/main/java/com/example/mtaa/Constats.java
+++ app/src/main/java/com/example/mtaa/Constats.java
@@ -2,6 +2,9 @@ package com.example.mtaa;
 
 public class Constats {
 
+    //MISO H - IP 192.168.0.116
+    //MISO J - IP
+
     private static final String ROOT_URL = "http://192.168.0.116/MTAA/";
     public static final String REGISTER_URL = ROOT_URL+"createUser.php";
     public static final String GET_ALL_RESTAURANTS_URL = ROOT_URL+"GetAllRestaurants.php";
