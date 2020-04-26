b373b52bda64d8635384aa9c9394f6da273da45b
Michal Heban <michal.heban52@gmail.com>
2020-04-26 18:57:50 +0200

    Login style finished

--- app/src/main/java/com/example/mtaa/Login.java
+++ app/src/main/java/com/example/mtaa/Login.java
@@ -1,8 +1,12 @@
 package com.example.mtaa;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +25,7 @@ import java.nio.charset.StandardCharsets;
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private EditText enterUserName, enterPassword;
+    private String userName, password;
     private Button loginBtn;
     private TextView forgotPasswordTextView, registerTextView;
     private ProgressDialog progressDialog;
@@ -36,6 +41,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         loginBtn = (Button) findViewById(R.id.LoginBtn);
         loginBtn.setOnClickListener(this);
+        loginBtn.setEnabled(false);
 
         forgotPasswordTextView = (TextView) findViewById(R.id.forgotPasswordTextView);
         registerTextView = (TextView) findViewById(R.id.registerTextView);
@@ -45,73 +51,72 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         progressDialog = new ProgressDialog(this);
         globals = (GlobalVariables) getApplicationContext();
+
+        enterUserName.addTextChangedListener(registerTextWatcher);
+        enterPassword.addTextChangedListener(registerTextWatcher);
     }
 
     private void loginUser(){
 
-        final String userName = enterUserName.getText().toString().trim();
-        final String userPassword = enterPassword.getText().toString().trim();
+        loginBtn.setEnabled(true);
+        progressDialog.setMessage("Prihlasujem");
+        progressDialog.show();
+        try{
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("username", userName);
+            jsonBody.put("password", password);
+            final String mRequestBody = jsonBody.toString();
 
-        if(!userName.isEmpty() && !userPassword.isEmpty()){
-            progressDialog.setMessage("Prihlasujem");
-            progressDialog.show();
-            try{
-                JSONObject jsonBody = new JSONObject();
-                jsonBody.put("username", userName);
-                jsonBody.put("password", userPassword);
-                final String mRequestBody = jsonBody.toString();
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, Constats.LOGIN_URL,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                progressDialog.dismiss();
-                                try{
-                                    JSONObject res = new JSONObject(response);
-                                    if(res.getString("response_code").equals("200")){
-                                        JSONObject data = res.getJSONObject("response_desc");
-                                        globals.setUserId(data.getString("id"));
-                                        if(globals.getUserId().equals("1")){
-                                            toAdmin();
-                                        }
-
-                                        else toWelocme(userName);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, Constats.LOGIN_URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            progressDialog.dismiss();
+                            try{
+                                JSONObject res = new JSONObject(response);
+                                if(res.getString("response_code").equals("200")){
+                                    JSONObject data = res.getJSONObject("response_desc");
+                                    globals.setUserId(data.getString("id"));
+                                    if(globals.getUserId().equals("1")){
+                                        toAdmin();
                                     }
-                                    else Toast.makeText(getApplicationContext(), res.getString("response_desc"), Toast.LENGTH_LONG).show();
-
-                                }catch (JSONException e){
-                                    e.printStackTrace();
+                                    else toWelocme(userName);
                                 }
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                progressDialog.hide();
-                                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                                System.out.println(error);
-                            }
-                        }){
-                    @Override
-                    public String getBodyContentType() {
-                        return "application/json; charset=utf-8";
-                    }
+                                else {
+                                    enterPassword.setError("Nesprávna kombinácia mena a hesla");
+                                    enterPassword.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                                }
 
-                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                    @Override
-                    public byte[] getBody() {
-                        return mRequestBody.getBytes(StandardCharsets.UTF_8);
-                    }
-                };
-                RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progressDialog.hide();
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                            System.out.println(error);
+                        }
+                    }){
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
 
-            }
-            catch (JSONException e) {
-                e.printStackTrace();
-            }
+                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                @Override
+                public byte[] getBody() {
+                    return mRequestBody.getBytes(StandardCharsets.UTF_8);
+                }
+            };
+            RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
 
         }
-        else Toast.makeText(getApplicationContext(), "Vyplň všetky polia!", Toast.LENGTH_LONG).show();
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -124,7 +129,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private void toForgotPassowrd() {
         Intent intent = new Intent(this, ResetPassword.class);
         startActivity(intent);
-        //finish();
     }
 
     private void toWelocme(String userName){
@@ -155,5 +159,18 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             toRegister();
         }
     }
+
+    private TextWatcher registerTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            userName = enterUserName.getText().toString().trim();
+            password = enterPassword.getText().toString().trim();
+            loginBtn.setEnabled(!userName.isEmpty() && !password.isEmpty());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {}
+    };
 
 }
